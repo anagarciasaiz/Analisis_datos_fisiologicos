9e8faf70a2f8e021e773f19c0f6b347d1d73c1ae
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-29 11:09:09 +0100

    dd

---
+++ Electromiograma/datos.py
@@ -0,0 +1,8 @@
+'''
+MUSCULAR
+- eventos (contraccion-relajacion)
+- amplitud señal: nivel activación muscular
+- frecuencia (fatiga muscular)
+- patrones activación muscular
+- distancia entre picos
+'''
