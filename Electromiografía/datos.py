f5b2fb0e71400b710e01add77ce3f13c52128569
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-29 12:07:09 +0100

    limpiar

--- Electromiografía/datos.py
+++ Electromiografía/datos.py
@@ -11,11 +11,11 @@ from sklearn.preprocessing import StandardScaler
 from scipy.signal import medfilt
 import os
 
-#obtenemos la ruta del archivo
+'''#obtenemos la ruta del archivo
 ruta = os.path.dirname(os.path.abspath(__file__))
 
 #Contruimos la ruta del archivo
-ruta_archivo = os.path.join(ruta, 'Andrea_Brazo_excel.csv')
+ruta_archivo = os.path.join(ruta, csv)
 
 # Cargar el archivo CSV en un DataFrame de pandas
 df = pd.read_csv(ruta_archivo, encoding='utf-8', delimiter=';')
@@ -36,4 +36,29 @@ scaler = StandardScaler()
 df['A1_normalizado'] = scaler.fit_transform(df[['A1_filtrado']])
 
 # Guardar los datos filtrados y normalizados en un nuevo archivo CSV
-df.to_csv('datos_emg_filtrados_normalizados.csv', index=False)
+df.to_csv('datos_emg_filtrados_normalizados.csv', index=False)'''
+
+def limpiar_csv(csv, columnas_eliminar):
+    #obtenemos la ruta del archivo
+    ruta = os.path.dirname(os.path.abspath(__file__))
+
+    #Contruimos la ruta del archivo
+    ruta_archivo = os.path.join(ruta, csv)
+
+    # Cargar el archivo CSV en un DataFrame de pandas
+    df = pd.read_csv(ruta_archivo, encoding='utf-8', delimiter=';')
+    df.columns = df.columns.str.strip()
+    if columnas_eliminar is not None:
+        df = df.drop(columnas_eliminar, axis = 1)
+    else:
+        pass
+    
+    return df
+
+df_andrea_brazo = limpiar_csv('Andrea_Brazo_excel.csv', ['A2', 'A3', 'A4', 'A5', 'A6'])
+print('ANDREA BRAZO')
+print(df_andrea_brazo.head())
+
+df_ana_brazo = limpiar_csv('brazo_Ana_excel.csv', None)
+print('ANA BRAZO')
+print(df_ana_brazo.head())
