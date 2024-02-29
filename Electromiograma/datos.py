a3ac80769346ac949e69211c6b7fed4ca4033d73
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-29 11:13:53 +0100

    cccc

--- Electromiograma/datos.py
+++ Electromiograma/datos.py
@@ -6,3 +6,19 @@ MUSCULAR
 - patrones activación muscular
 - distancia entre picos
 '''
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from scipy.signal import medfilt
+
+# Cargar el archivo CSV en un DataFrame de pandas
+df = pd.read_csv('Andrea_Brazo_excl.csv')
+
+# Aplicar filtro de mediana para el filtrado de ruido
+df['emg_filtrado'] = medfilt(df['emg'], kernel_size=3)
+
+# Normalizar los datos utilizando normalización z-score
+scaler = StandardScaler()
+df['emg_normalizado'] = scaler.fit_transform(df[['emg_filtrado']])
+
+# Guardar los datos filtrados y normalizados en un nuevo archivo CSV
+df.to_csv('datos_emg_filtrados_normalizados.csv', index=False)
