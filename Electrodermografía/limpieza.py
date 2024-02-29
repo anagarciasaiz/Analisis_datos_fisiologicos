0cc40e46f05ec46ca6f74748182a729a1c373867
Ana López <anamuyamiga@gmail.com>
2024-02-29 11:54:41 +0100

    filtrado y normalizacion completa

--- Electrodermografía/limpieza.py
+++ Electrodermografía/limpieza.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 from scipy.signal import medfilt
+from sklearn.preprocessing import StandardScaler
 
 # cargamos el csv
 ruta = os.path.dirname(os.path.abspath(__file__))
@@ -12,9 +13,11 @@ df_andmanorep = pd.read_csv(ruta_archivo, encoding='utf-8', delimiter=';')
 
 # Filtrado de ruido de mediana 
 window_size = 3 #Tamaño de la ventana 
-df_andmanorep['A3_filtered'] = medfilt(df_andmanorep['A3'], kernel_size=window_size)
+df_andmanorep['A3_filtrado'] = medfilt(df_andmanorep['A3'], kernel_size=window_size)
 
 # Normalizar los datos utilizando normalización z-score
-col_normalizar = df_andmanorep.columns.difference(['nSeq'])
-df_andmanorep[col_normalizar] = (df_andmanorep[col_normalizar] - df_andmanorep[col_normalizar].mean()) / df_andmanorep[col_normalizar].std()
+scaler = StandardScaler()
+df_andmanorep['A3_normalizado'] = scaler.fit_transform(df_andmanorep[['A3_filtrado']])
+df_andmanorep.to_csv('datos_edg_filtrados_normalizados.csv', index=False)
+
 
