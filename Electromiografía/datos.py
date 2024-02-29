26e3b4cc96146bd31acc46ae6520ba214773d1c4
andmansim <91722324+andmansim@users.noreply.github.com>
2024-02-29 12:13:53 +0100

    listo

--- Electromiografía/datos.py
+++ Electromiografía/datos.py
@@ -11,36 +11,9 @@ from sklearn.preprocessing import StandardScaler
 from scipy.signal import medfilt
 import os
 
-'''#obtenemos la ruta del archivo
-ruta = os.path.dirname(os.path.abspath(__file__))
-
-#Contruimos la ruta del archivo
-ruta_archivo = os.path.join(ruta, csv)
-
-# Cargar el archivo CSV en un DataFrame de pandas
-df = pd.read_csv(ruta_archivo, encoding='utf-8', delimiter=';')
-df.columns = df.columns.str.strip()
-print('COLUMNAS-------------')
-print(df.columns)
-
-df.columns = ['nSeq', 'I1', 'I2', 'O1', 'O2','A1', 'A2', 'A3', 'A4', 'A5', 'A6']
-columnas_eliminar =['A2', 'A3', 'A4', 'A5', 'A6']
-df = df.drop(columnas_eliminar, axis = 1)
-print(df.head())
-
-# Aplicar filtro de mediana para el filtrado de ruido
-df['A1_filtrado'] = medfilt(df['A1'], kernel_size=3)
-
-# Normalizar los datos utilizando normalización z-score
-scaler = StandardScaler()
-df['A1_normalizado'] = scaler.fit_transform(df[['A1_filtrado']])
-
-# Guardar los datos filtrados y normalizados en un nuevo archivo CSV
-df.to_csv('datos_emg_filtrados_normalizados.csv', index=False)'''
 
 def limpiar_csv(csv, columnas_eliminar):
-    #obtenemos la ruta del archivo
-    ruta = os.path.dirname(os.path.abspath(__file__))
+    
 
     #Contruimos la ruta del archivo
     ruta_archivo = os.path.join(ruta, csv)
@@ -55,15 +28,18 @@ def limpiar_csv(csv, columnas_eliminar):
     
     return df
 
-def ruido_normalizacion(df, columna):
+def ruido_normalizacion(df, columna, nombre):
     # Aplicar filtro de mediana para el filtrado de ruido
     df[columna + '_filtrado'] = medfilt(df[columna], kernel_size=3)
 
     # Normalizar los datos utilizando normalización z-score
     scaler = StandardScaler()
     df[columna + '_normalizado'] = scaler.fit_transform(df[[columna + '_filtrado']])
-
+    df.to_csv(os.path.join(ruta, nombre), index=False)
     return df
+
+#obtenemos la ruta del archivo
+ruta = os.path.dirname(os.path.abspath(__file__))
 
 # Cargar los datos de los archivos CSV y los limpiamos
 df_andrea_brazo = limpiar_csv('Andrea_Brazo_excel.csv', ['A2', 'A3', 'A4', 'A5', 'A6'])
@@ -75,14 +51,13 @@ print('ANA BRAZO')
 print(df_ana_brazo.head())
 
 # Aplicar filtro de mediana para el filtrado de ruido y normalizamos
-df_andrea_brazo = ruido_normalizacion(df_andrea_brazo, 'A1')
+df_andrea_brazo = ruido_normalizacion(df_andrea_brazo, 'A1', 'brazo_Andrea_filtrado_normalizado.csv')
 print('ANDREA BRAZO FILTRADO Y NORMALIZADO')
 print(df_andrea_brazo.head())
 
-df_ana_brazo = ruido_normalizacion(df_ana_brazo, 'A1')
+df_ana_brazo = ruido_normalizacion(df_ana_brazo, 'A1', 'brazo_Ana_filtrado_normalizado.csv')
 print('ANA BRAZO FILTRADO Y NORMALIZADO')
 print(df_ana_brazo.head())
 
-# Guardar los datos filtrados y normalizados en un nuevo archivo CSV
-df_andrea_brazo.to_csv('andrea_brazo_filtrado_normalizado.csv', index=False)
-df_ana_brazo.to_csv('ana_brazo_filtrado_normalizado.csv', index=False)
+
+
